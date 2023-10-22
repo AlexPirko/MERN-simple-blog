74f891b1182090e3d424bfa818f811784abdd84d
Алекс Южный <youzhny.foto@gmail.com>
2023-10-22 17:26:55 +0300

    add check auth func, refactor some code

---
+++ controllers/UserController.js
@@ -0,0 +1,114 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
+
+import UserSchema from '../models/User.js';
+
+export const register = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserSchema({
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: hash,
+            avatarUrl: req.body.avatarUrl,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret11',
+            {
+                expiresIn: '30d',
+            },
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to register',
+        });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const user = await UserSchema.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User don't found",
+            });
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Wrong login or password',
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            },
+            'secret11',
+            {
+                expiresIn: '30d',
+            },
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to authorisation',
+        });
+    }
+};
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await UserSchema.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+            });
+        }
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            userData,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'No access',
+        });
+    }
+};
