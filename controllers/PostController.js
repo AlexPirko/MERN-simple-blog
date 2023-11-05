ed7b905d901b8748beec4bab388eee34417a8b46
Алекс Южный <youzhny.foto@gmail.com>
2023-11-05 19:01:10 +0200

    add cors and post tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,6 +13,24 @@ export const getAll = async (req, res) => {
     }
 };
 
+export const getLastTags = async (req, res) => {
+    try {
+        const posts = await PostSchema.find().limit(5).exec();
+
+        const tags = posts
+            .map((obj) => obj.tags)
+            .flat()
+            .slice(0, 5);
+
+        res.json(tags);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get tags',
+        });
+    }
+};
+
 export const getOne = async (req, res) => {
     try {
         const postId = req.params.id;
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import fs from 'fs';
 import multer from 'multer';
 import mongoose from 'mongoose';
 import { loginValidator, postCreateValidator, registerValidator } from './validations/validations.js';
 
 import checkAuth from './utils/checkAuth.js';
-import { getAll, getOne, create, update, removeOne } from './controllers/PostController.js';
+import { getAll, getLastTags, getOne, create, update, removeOne } from './controllers/PostController.js';
 import { register, login, getMe } from './controllers/UserController.js';
 import handleValidationErrors from './utils/handleValidationErrors.js';
 
 mongoose
-    .connect('mongodb+srv://youzhnyfoto:Cfif1234@cluster0.ppu5twh.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect('mongodb+srv://youzhnyfoto:Cfif1234@cluster0.ppu5twh.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('db ok'))
     .catch((err) => console.log('db error', err));
 
@@ -34,6 +35,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.use(cors());
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
@@ -47,7 +50,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     });
 });
 
+app.get('/tags', getLastTags);
+
 app.get('/posts', getAll);
+app.get('/posts/tags', getLastTags);
 app.get('/posts/:id', getOne);
 app.post('/posts', checkAuth, postCreateValidator, handleValidationErrors, create);
 app.patch('/posts/:id', checkAuth, postCreateValidator, handleValidationErrors, update);
